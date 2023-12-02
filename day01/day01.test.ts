0610e7543c79872bff039d04f54d818b2dd107c0
Isma Navarro <ismanapa@gmail.com>
2023-12-02 22:18:07 +0100

    feat: day 01 part 2

--- day01/day01.test.ts
+++ day01/day01.test.ts
@@ -1,15 +1,22 @@
 import { day1 } from "./day01";
 
 describe("Day 01", () => {
-    test("Works with test input", () => {
-        const result = day1("test");
+    test("Works with test input 1", () => {
+        const result = day1("test1");
         
         expect(result).toBe(142);
     })
 
-    test("Shows answer of input1", () => {
+    test("Works with test input 2", () => {
+        const result = day1("test2");
+        
+        expect(result).toBe(281);
+    })
+
+    test.only("Shows answer of input", () => {
         const result = day1("input");
 
-        console.log("result day 01 - part 1:", result);
+        console.log("result day 01:", result);
     })
+
 });
--- day01/day01.ts
+++ day01/day01.ts
@@ -1,20 +1,80 @@
 import { readFileLines } from "../utils";
 
-export function day1(mode: "test" | "input") {
+export function day1(mode: "test1" | "test2" | "input") {
   const data = readFileLines(`${__dirname}/${mode}.txt`);
 
-  return data.map((line) => {
-    return parseInt(`${getNumberFromLeft(line)}${getNumberFromRight(line)}`)
-  })
-  .reduce((val, acc) => (val+acc), 0);
+  return data
+    .map((line) => {
+      const formattedLine = convertStringsToNumbers(sanitize(line));
+      const value = parseInt(
+        `${getNumberFromLeft(formattedLine)}${getNumberFromRight(
+          formattedLine
+        )}`
+      );
+      return value;
+    })
+    .reduce((val, acc) => val + acc, 0);
 }
 
 function getNumberFromLeft(line: string): string {
-    const el = line.split("").find(char => !isNaN(parseInt(char)));
-    return el!;
+  const el = line.split("").find((char) => !isNaN(parseInt(char)));
+  return el!;
 }
 
 function getNumberFromRight(line: string): string {
-    const el = line.split("").reverse().find(char => !isNaN(parseInt(char)));
-    return el!;
+  const el = line
+    .split("")
+    .reverse()
+    .find((char) => !isNaN(parseInt(char)));
+  return el!;
+}
+
+function sanitize(input: string): string {
+    return input
+        .replaceAll('eightwo', 'eighttwo')
+        .replaceAll('twone', 'twoone')
+        .replaceAll('nineight', 'nineeight')
+        .replaceAll('threeight', 'threeeight')
+        .replaceAll('sevenine', 'sevennine')
+        .replaceAll('oneight', 'oneeight')
+        .replaceAll('fiveight', 'fiveeight')
+}
+
+function convertStringsToNumbers(line: string): string {
+  const numbers = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  let indexes = getIndexesOfNumber(line, numbers);
+
+  while (indexes.some((i) => i != -1)) {
+    const lowerIndexPosition = indexes.indexOf(
+      Math.min(...indexes.filter((i) => i !== -1))
+    );
+    line = line.replace(
+      numbers[lowerIndexPosition],
+      (lowerIndexPosition + 1).toString()
+    );
+    indexes = getIndexesOfNumber(line, numbers);
+  }
+
+  return line;
+}
+
+function getIndexesOfNumber(line: string, numbers: string[]): number[] {
+  const indexes: number[] = [];
+
+  numbers.forEach((number, index) => {
+    indexes[index] = line.indexOf(number);
+  });
+
+  return indexes;
 }
