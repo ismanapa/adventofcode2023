b81d57e95b826384fc19852eb9f464915044279d
Isma Navarro <ismanapa@gmail.com>
2023-12-03 18:06:30 +0100

    feat: day 3 - part 1

--- day03/day03_1.test.ts
+++ day03/day03_1.test.ts
@@ -1,9 +1,15 @@
 import { day03_1 } from "./day03_1";
 
 describe("Day 03", () => {
-    test("Works with test input 1", () => {
-        const result = day03_1("test1");
-        
-        expect(result).toBe(4361);
-    });
+  test("Works with test input 1", () => {
+    const result = day03_1("test1");
+
+    expect(result).toBe(4361);
+  });
+
+  test("Show answer of puzzle 1", () => {
+    const result = day03_1("input");
+
+    console.log("result day 03 - part 1:", result);
+  });
 });
--- day03/day03_1.ts
+++ day03/day03_1.ts
@@ -1,7 +1,81 @@
 import { readFileLines } from "../utils";
 
-export function day03_1(mode: "test1" | "input"){
-  const data = readFileLines(`${__dirname}/${mode}.txt`);
+type Symbol = {
+  symbol: string;
+  rowIndex: number;
+  index: number;
+};
 
-    return 0;
+type Number = {
+  number: number;
+  row: number;
+  position: number;
+  endPosition: number;
+};
+
+export function day03_1(mode: "test1" | "input") {
+  const data = readFileLines(`${__dirname}/${mode}.txt`);
+  const symbols = findSymbolPositions(data);
+
+  return data
+    .reduce<Number[]>((acc, row, rowIndex) => {
+      const numbers = mapNumbersInfoInRow(row, rowIndex);
+
+      if (numbers.length == 0) {
+        return acc;
+      }
+
+      const parts = numbers.filter((number) => {
+        const isPart = symbols.some((symbol) => {
+          return (
+            symbol.rowIndex >= number.row - 1 &&
+            symbol.rowIndex <= number.row + 1 &&
+            symbol.index >= number.position - 1 &&
+            symbol.index <= number.endPosition + 1
+          );
+        });
+
+        return isPart;
+      });
+
+      return acc.concat(...parts);
+    }, [])
+    .reduce((acc, enginePart) => {
+      return acc + enginePart.number;
+    }, 0);
 }
+
+const mapNumbersInfoInRow = (row: string, rowIndex: number) => {
+  const regex = /\d+/g;
+  const matches: Number[] = [];
+  let match;
+
+  while ((match = regex.exec(row)) !== null) {
+    const number = match[0];
+    const position = match.index;
+    matches.push({
+      number: parseInt(number),
+      position,
+      endPosition: position + number.length - 1,
+      row: rowIndex,
+    });
+  }
+
+  return matches;
+};
+
+const findSymbolPositions = (input: string[]) => {
+  const positions: Symbol[] = [];
+
+  input.forEach((row, rowIndex) => {
+    const items = row.split("");
+
+    items.forEach((item, index) => {
+      if (item.match(/[^\w.]/)) {
+        positions.push({ rowIndex, index, symbol: item });
+      }
+    });
+  });
+
+  return positions;
+};
